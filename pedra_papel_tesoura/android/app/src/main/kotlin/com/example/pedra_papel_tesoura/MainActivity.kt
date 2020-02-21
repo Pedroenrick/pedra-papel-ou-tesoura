12edb62d6d162729dc35b210be6586411f267d91
Pedroenrick <pedro_penna22@hotmail.com>
2020-02-21 19:24:07 -0300

    Jogo pedra, papel ou tesoura

---
+++ pedra_papel_tesoura/android/app/src/main/kotlin/com/example/pedra_papel_tesoura/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pedra_papel_tesoura
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
